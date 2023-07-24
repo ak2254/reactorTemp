694e551e95674c8dad0806877763b6a8429ddb45
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-07-24 16:50:22 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,17 +1,8 @@
-// Step 1: Create a new collection to store the distinct values and their IDs
-ClearCollect(DistinctTable, {})
+ClearCollect(BigCollection, {}); // Create an empty collection to store the grouped data
 
-// Step 2: Iterate through the "srecords" table to find distinct values and assign IDs
-ForAll(srecords,
-    {
-        // Extract the value from the "k" column (you can modify this part based on the data type of "k" column)
-        "Value": ThisItem.k,
-        // Add an integer ID starting from 1 for each distinct value
-        "ID": If(
-                 IsBlank(LookUp(DistinctTable, Value = ThisItem.k, ID)),
-                 CountRows(DistinctTable) + 1,
-                 LookUp(DistinctTable, Value = ThisItem.k, ID)
-             )
-    }
-)
-
+ForAll(GroupedEmployees,
+    Collect(BigCollection, {
+        Department: Result.Department,
+        Employees: Result
+    })
+);
