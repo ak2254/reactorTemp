70b8fabb36482acf782374084476b69750a9bab5
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-07-24 14:44:43 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,18 +1,2 @@
-// Clear the BigCollection if it already exists
-ClearCollect(BigCollection, {});
-
-// Create a variable to store the chunk size (e.g., 1000 records per chunk)
-Set(chunkSize, 1000);
-
-// Calculate the total number of records in the data source
-Set(totalRecords, CountRows(YourDataSource));
-
-// Use ForAll to loop through the data in chunks
-ForAll(
-    Split(YourDataSource, chunkSize), // Split the data source into chunks
-    // Loop body
-    Collect(
-        BigCollection,
-        ThisRecord // Add the chunk of data to the BigCollection
-    )
-)
+# Convert the "prep_date" column to the desired "yymm" format
+df['yymm'] = df['prep_date'].apply(lambda x: f'{str(x.year)[-2:]}{x.strftime("%d")}')
