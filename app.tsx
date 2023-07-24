dd6642b876e0bfd480756e15df71d174c130ae2c
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-07-24 15:37:03 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,2 +1,17 @@
-# Convert the "prep_date" column to the desired "yymm" format
-df['yymm'] = df['prep_date'].apply(lambda x: f'{str(x.year)[-2:]}{x.strftime("%d")}')
+// Step 1: Create a new collection to store the distinct values and their IDs
+ClearCollect(DistinctTable, {})
+
+// Step 2: Iterate through the "srecords" table to find distinct values and assign IDs
+ForAll(srecords,
+    {
+        // Extract the value from the "k" column (you can modify this part based on the data type of "k" column)
+        "Value": ThisItem.k,
+        // Add an integer ID starting from 1 for each distinct value
+        "ID": If(
+                 IsBlank(LookUp(DistinctTable, Value = ThisItem.k, ID)),
+                 CountRows(DistinctTable) + 1,
+                 LookUp(DistinctTable, Value = ThisItem.k, ID)
+             )
+    }
+)
+
