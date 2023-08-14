ab0941aa37e45e982b9f0e1b51d23a55d93cefdc
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-08-14 16:17:13 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,12 +1,27 @@
-from shareplum import Site, Office365
-import pandas as pd
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from datetime import datetime
 
-# SharePoint site and list information
-sharepoint_url = "https://your-sharepoint-site-url"
-username = "your-username"
-password = "your-password"
-list_name = "Your List Name"
+# Specify the path to your Excel file
+excel_file_path = "path/to/your/excel/file.xlsx"
 
-# Connect to SharePoint using Office365 authentication
-authcookie = Office365(sharepoint_url, username=username, password=password).GetCookies()
-site = Site(sharepoint_url, version=Version.v365, authcookie=authcookie)
+# Load the existing Excel file
+wb = openpyxl.load_workbook(excel_file_path)
+
+# Select the worksheet you want to work with
+ws = wb.active
+
+# Calculate the new values for the row to be inserted
+last_row = ws.max_row
+new_id = last_row + 1
+new_value = 50
+new_version = ws.cell(row=last_row, column=3).value + 1
+new_timestamp = datetime.now()
+
+# Create a new row
+new_row_data = [new_id, new_value, new_version, new_timestamp]
+ws.append(new_row_data)
+
+# Save the updated Excel file
+wb.save(excel_file_path)
