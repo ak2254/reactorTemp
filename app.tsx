a90b1b4e4d7936f5f6fbb5a8c4038952f47c3eb3
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-04 11:01:35 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,33 +1,14 @@
-
 WITH AlarmCounts AS (
     SELECT
         [Tag],
         [AlaramType],
         MIN([AlaramTime]) AS [FirstAlarmTime],
         COUNT(*) AS [Count]
-    FROM YourExistingTable
+    FROM ' + SUBSTRING(@tablePath, 1, (LEN(@tablePath) - CHARINDEX('[', REVERSE(@tablePath)))) + '[_ClearedAlrams] c
     WHERE
-        [AlarmTime] BETWEEN @startDate AND DATEADD(HOUR, 24, @startDate)
-        AND ([SeqNo2] IS NOT NULL OR [Time2] IS NULL) -- Include alarms without a clear time
+        c.[AlarmTime] BETWEEN @startDate AND DATEADD(HOUR, 24, @startDate)
+        AND (c.[SeqNo2] IS NOT NULL OR c.[Time2] IS NULL) -- Include alarms without a clear time
     GROUP BY
         [Tag],
         [AlaramType]
 )
-UPDATE YourExistingTable
-SET [ab3] = CASE
-    WHEN ac.[Count] > 3 THEN 'yes'
-    ELSE ''
-    END
-FROM YourExistingTable AS c
-LEFT JOIN AlarmCounts ac
-    ON ac.[Tag] = c.[Tag]
-    AND ac.[AlaramType] = c.[AlaramType]
-    AND ac.[FirstAlarmTime] = c.[AlaramTime]
-WHERE
-    [AlarmTime] BETWEEN @startDate AND DATEADD(HOUR, 24, @startDate)
-    AND (
-        [SeqNo2] IS NOT NULL
-        OR [Time2] IS NULL
-        OR (ac.[Count] > 3)
-    )
-
