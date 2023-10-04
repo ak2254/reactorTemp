9f5aff26e297917b51c3fda427657cf1503c625d
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-04 10:51:20 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,19 +1,33 @@
 
-import pandas as pd
+WITH AlarmCounts AS (
+    SELECT
+        [Tag],
+        [AlaramType],
+        MIN([AlaramTime]) AS [FirstAlarmTime],
+        COUNT(*) AS [Count]
+    FROM YourExistingTable
+    WHERE
+        [AlarmTime] BETWEEN @startDate AND DATEADD(HOUR, 24, @startDate)
+        AND ([SeqNo2] IS NOT NULL OR [Time2] IS NULL) -- Include alarms without a clear time
+    GROUP BY
+        [Tag],
+        [AlaramType]
+)
+UPDATE YourExistingTable
+SET [ab3] = CASE
+    WHEN ac.[Count] > 3 THEN 'yes'
+    ELSE ''
+    END
+FROM YourExistingTable AS c
+LEFT JOIN AlarmCounts ac
+    ON ac.[Tag] = c.[Tag]
+    AND ac.[AlaramType] = c.[AlaramType]
+    AND ac.[FirstAlarmTime] = c.[AlaramTime]
+WHERE
+    [AlarmTime] BETWEEN @startDate AND DATEADD(HOUR, 24, @startDate)
+    AND (
+        [SeqNo2] IS NOT NULL
+        OR [Time2] IS NULL
+        OR (ac.[Count] > 3)
+    )
 
-# Assuming you have DataFrames df1 and df2
-# Rename the "timestamp" column in df1 to "timestamp_sheet1"
-df1 = df1.rename(columns={"timestamp": "timestamp_sheet1"})
-
-# Rename the "timestamp" column in df2 to "timestamp_sheet2"
-df2 = df2.rename(columns={"timestamp": "timestamp_sheet2"})
-
-# Merge df1 and df2 on the "timestamp_sheet1" and "timestamp_sheet2" columns with a left join
-merged_df = pd.merge(df1, df2, left_on="timestamp_sheet1", right_on="timestamp_sheet2", how="left")
-
-# Find rows in df1 that are not in df2
-rows_not_in_df2 = merged_df[merged_df["timestamp_sheet2"].isna()]
-
-# Now, rows_not_in_df2 contains the rows from df1 that are not in df2 based on the "timestamp" column
-print(rows_not_in_df2)
-filtered_df = merged_df[merged_df["alarm type"] != "return"]
