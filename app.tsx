aadfe72e750452174aa024608417ea5b336bf8e9
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-04 16:16:13 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,17 +1,23 @@
- SELECT
-    tag,
-    alarm_type,
-    count(*) AS occurrence_count,
-    CASE WHEN occurrence_count > 3 OR past_24_hour_occurrence_count > 3 THEN 'Flagged' ELSE 'Not Flagged' END AS [abthree]
-FROM
-    @TimeInAlaramResults
-GROUP BY
-    tag,
-    alarm_type
-HAVING
-    occurrence_count > 3
-    AND alarm_time <= end_of_day
-ORDER BY
-    tag,
-    alarm_type,
-    alarm_time ASC;
+UPDATE @TimeInAlaramResults
+SET [flagged] = CASE WHEN occurrence_count > 3 OR past_24_hour_occurrence_count > 3 THEN 'flagged' ELSE 'not flagged' END
+FROM (
+    SELECT
+        tag,
+        AlarmType,
+        count(*) AS occurrence_count,
+        (
+            SELECT COUNT(*)
+            FROM @TimeInAlaramResults c2
+            WHERE c2.[tag] = c.[tag]
+                AND c2.[AlarmType] = c.[AlarmType]
+                AND c2.[AlarmTime] BETWEEN DATEADD(HOUR, -24, c.[AlarmTime]) AND c.[AlarmTime]
+                AND c2.[AlarmTime] <= c.[AlarmTime]
+        ) AS past_24_hour_occurrence_count
+    FROM
+        @TimeInAlaramResults
+    GROUP BY
+        tag,
+        AlarmType
+    HAVING
+        occurrence_count > 3
+) AS t
