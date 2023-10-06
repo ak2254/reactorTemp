7559ed3e3c4c8a655140fabdee67f062d0f88fca
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-06 15:10:24 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,16 +1,16 @@
-UPDATE t
-SET [abthree] = 'Flagged'
-FROM @TimeInAlaramResults t
-WHERE (
-    SELECT COUNT(*)
+ WHERE (
+    SELECT COUNT(*) 
     FROM @TimeInAlaramResults c2
     WHERE c2.[tag] = t.[tag]
     AND c2.[AlaramType] = t.[AlaramType]
     AND c2.[AlarmTime] BETWEEN DATEADD(HOUR, -24, t.[AlarmTime]) AND t.[AlarmTime]
-    AND c2.[AlarmTime] BETWEEN @StartDate AND @EndDate
-    GROUP BY c2.[tag], c2.[AlaramType]
-    HAVING COUNT(*) >= 3
+) >= 3
+OR (
+    SELECT COUNT(*) 
+    FROM @TimeInAlaramResults c2
+    WHERE c2.[tag] = t.[tag]
+    AND c2.[AlaramType] = t.[AlaramType]
+    AND c2.[AlarmTime] <= DATEADD(HOUR, -24, t.[AlarmTime])
+    AND c2.[AlarmTime] + INTERVAL 24 HOUR >= t.[AlarmTime]
 ) >= 3;
-
-
 
