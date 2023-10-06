c93a481ef8a5713a12ce0cb9e6fefc28b95d74f3
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-06 14:48:36 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -8,18 +8,9 @@ WHERE (
     AND c2.[AlaramType] = t.[AlaramType]
     AND c2.[AlarmTime] BETWEEN DATEADD(HOUR, -24, t.[AlarmTime]) AND t.[AlarmTime]
     AND c2.[AlarmTime] BETWEEN @StartDate AND @EndDate
-) >= 3
-OR (
-    SELECT COUNT(*)
-    FROM @TimeInAlaramResults c3
-    WHERE c3.[tag] = t.[tag]
-    AND c3.[AlaramType] = t.[AlaramType]
-    AND c3.[AlarmTime] BETWEEN DATEADD(HOUR, -24, t.[AlarmTime]) AND t.[AlarmTime]
-    AND c3.[AlarmTime] BETWEEN @StartDate AND @EndDate
-) = 2
-This updated code checks if there are at least three alarms (including the current one) within the last 24 hours and flags all of them. It also checks if there are exactly two alarms within the last 24 hours and flags them as well. This way, it ensures that all three alarms are flagged when the criteria are met.
-
-
+    GROUP BY c2.[tag], c2.[AlaramType]
+    HAVING COUNT(*) >= 3
+) >= 3;
 
 
 
