18f1e27de5d249442ee09f72415a15c947fbf913
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-06 11:47:51 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,10 +1,25 @@
-UPDATE timeInAlaramResults
-SET [flagged] = (
-    CASE WHEN (
-        SELECT COUNT(*)
-        FROM ' + SUBSTRING(@tablePath, 1, (LEN(@tablePath) - CHARINDEX('[', REVERSE(@tablePath)))) + '[_ClearedAlarms] c2
-        WHERE c2.[tag] = c.[tag]
-            AND c2.[AlaramType] = c.[AlaramType]
-            AND c2.[AlarmTime] <= c.[AlarmTime]
-    ) >= 3 THEN ''Flagged'' ELSE ''Not Flagged'' END
-)
+UPDATE t
+SET [abthree] = 'Flagged'
+FROM @TimeInAlaramResults t
+WHERE (
+    SELECT COUNT(*)
+    FROM @TimeInAlaramResults c2
+    WHERE c2.[tag] = t.[tag]
+    AND c2.[AlaramType] = t.[AlaramType]
+    AND c2.[AlarmTime] BETWEEN DATEADD(HOUR, -24, t.[AlarmTime]) AND t.[AlarmTime]
+    AND c2.[AlarmTime] BETWEEN @StartDate AND @EndDate
+) >= 3
+OR (
+    SELECT COUNT(*)
+    FROM @TimeInAlaramResults c3
+    WHERE c3.[tag] = t.[tag]
+    AND c3.[AlaramType] = t.[AlaramType]
+    AND c3.[AlarmTime] BETWEEN DATEADD(HOUR, -24, t.[AlarmTime]) AND t.[AlarmTime]
+    AND c3.[AlarmTime] BETWEEN @StartDate AND @EndDate
+) = 2
+This updated code checks if there are at least three alarms (including the current one) within the last 24 hours and flags all of them. It also checks if there are exactly two alarms within the last 24 hours and flags them as well. This way, it ensures that all three alarms are flagged when the criteria are met.
+
+
+
+
+
