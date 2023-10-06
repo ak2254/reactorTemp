f16929008787e3914f2b3e3e1d998b0c366ab261
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-06 11:02:11 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,1 +1,10 @@
-
+UPDATE t
+SET [abthree] = 'Flagged'
+FROM @TimeInAlaramResults t
+WHERE (
+    SELECT COUNT(*)
+    FROM @TimeInAlaramResults c2
+    WHERE c2.[tag] = t.[tag]
+    AND c2.[AlaramType] = t.[AlaramType]
+    AND c2.[AlarmTime] BETWEEN @StartDate AND @EndDate
+) > 3
