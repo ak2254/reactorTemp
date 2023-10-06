06472f83573a038b71174457f2c7718f495363c6
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-06 11:14:58 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,10 +1,11 @@
 UPDATE t
 SET [abthree] = 'Flagged'
 FROM @TimeInAlaramResults t
-WHERE (
-    SELECT COUNT(*)
+WHERE EXISTS (
+    SELECT 1
     FROM @TimeInAlaramResults c2
     WHERE c2.[tag] = t.[tag]
     AND c2.[AlaramType] = t.[AlaramType]
     AND c2.[AlarmTime] BETWEEN @StartDate AND @EndDate
-) > 3
+    AND [abthree] = 'Flagged'
+)
