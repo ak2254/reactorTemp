0708a1cfff76d519d348f69d80d90df1ffb15db2
ak2254 <51174202+ak2254@users.noreply.github.com>
2023-10-23 15:06:01 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -2,8 +2,26 @@ ClearCollect(CombinedCollection,
     ForAll(DataSet1, // Iterate through the first dataset
         ForAll(Filter(DataSet2, Project = DataSet1.Project && Resource = DataSet1.Resource), // Filter the second dataset based on Project and Resource
             If(taskType = "daily", // Check if the taskType is "daily"
-                For(var i = DateAdd(DateValue(DataSet1.[Start Date]), daystart - 1), i <= DataSet1.[End Date], i + 1, // Create daily samples starting from the "start day"
-                    { "Sample date": i, "SAP": SAP, "Project": Project, "Sample no": SampleNo }
+                ForAll(
+                    Filter(
+                        AddColumns(
+                            AddColumns(
+                                AddColumns(
+                                    Sequence(
+                                        DateDiff(DateValue(DataSet1.[Start Date]), DateValue(DataSet1.[End Date]), Days) + 1,
+                                        DateValue(DataSet1.[Start Date])
+                                    ),
+                                    "SAP", SAP,
+                                    "Project", Project,
+                                    "Sample no", SampleNo
+                                ),
+                                "Start Date", DateAdd(DateValue(DataSet1.[Start Date]), daystart - 1, Days)
+                            ),
+                            "Sample date", DateAdd([Start Date], Value(Text(ThisItem.Value, "[$-en-US]")), Days)
+                        ),
+                        true
+                    ),
+                    { "Sample date": [Sample date], "SAP": SAP, "Project": Project, "Sample no": SampleNo }
                 ),
                 { "Sample date": DateAdd(DateValue(DataSet1.[End Date]), -DayStart), "SAP": SAP, "Project": Project, "Sample no": SampleNo } // Create last day samples
             )
