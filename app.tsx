06fec588cd68fa4dbfa79aef122b14bf9f1aaae3
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-08-05 13:47:17 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,1 +1,41 @@
- Find(",", OriginalName) > 0 && Find(" ", OriginalName, Find(",", OriginalName) + 2) > 0 && Find(" ", OriginalName, Find(",", OriginalName) + 2) > Find(",", OriginalName),
+HierarchyTable = 
+VAR AllUsers = DISTINCT(TrainingData[UserID])
+VAR AllManagers = DISTINCT(Managers[ManagerID])
+VAR Combined = UNION(
+    SELECTCOLUMNS(
+        TrainingData,
+        "ManagerID", TrainingData[ManagerID],
+        "UserID", TrainingData[UserID]
+    ),
+    SELECTCOLUMNS(
+        TrainingData,
+        "ManagerID", BLANK(), // Blank if the UserID is not a Manager
+        "UserID", TrainingData[ManagerID]
+    )
+)
+VAR RecursiveHierarchy = 
+    GENERATE(
+        Combined,
+        VAR CurrentUser = [UserID]
+        VAR ManagerChain = PATH(
+            MAXX(
+                FILTER(
+                    Combined,
+                    Combined[UserID] = CurrentUser
+                ),
+                Combined[ManagerID]
+            ),
+            CurrentUser
+        )
+        RETURN
+            ADDCOLUMNS(
+                FILTER(
+                    Combined,
+                    PATHCONTAINS(ManagerChain, Combined[UserID])
+                ),
+                "PathUserID", [UserID],
+                "PathManagerID", [ManagerID]
+            )
+    )
+RETURN
+    RecursiveHierarchy
