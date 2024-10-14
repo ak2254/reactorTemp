093f9a025f316ac0041e424361e20072b614cea8
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-10-14 19:40:27 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,3 +1,13 @@
-"""
-    
-    """
+TotalObservations = 
+CALCULATE(
+    COUNTROWS('Observation'),
+    'Observation'[Complete] = "Yes",
+    'Observation'[Observer Name] = SELECTEDVALUE('Personnel'[Full Name]),
+    'Observation'[Date] >= SELECTEDVALUE('Personnel'[Start]),
+    'Observation'[Date] <= IF(
+         ISBLANK(SELECTEDVALUE('Personnel'[End])), 
+         TODAY(), 
+         SELECTEDVALUE('Personnel'[End])
+    )
+)
+
