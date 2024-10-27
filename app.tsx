1beb64d7717e716285aa5eb9f6b41c7704b9047d
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-10-26 23:01:57 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,1 +1,16 @@
+Not Completed Areas = 
+CALCULATE(
+    DISTINCTCOUNT(AreaTable[Area]),
+    EXCEPT(
+        VALUES(AreaTable[Area]),
+        CALCULATETABLE(
+            VALUES(AuditTable[Area]),
+            FILTER(
+                ALL(AuditTable),
+                YEAR(AuditTable[AuditDate]) = YEAR(MAX(AuditTable[AuditDate])) &&
+                MONTH(AuditTable[AuditDate]) = MONTH(MAX(AuditTable[AuditDate]))
+            )
+        )
+    )
+)
 
