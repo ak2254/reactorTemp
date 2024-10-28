500186ffc4dea73b676ebb9b56ecdd2c555696c7
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-10-28 18:52:17 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -68,4 +68,27 @@ CALCULATE(
         LOOKUPVALUE('personnel'[Role], 'personnel'[Full Name], 'audits'[Full Name], 'personnel'[Start Date], 'audits'[Observation Date]) = 'personnel'[Role]
     )
 )
+TotalObservationsWithRole = 
+CALCULATE(
+    COUNTROWS('audits'),
+    FILTER(
+        'audits',
+        'audits'[Completed] = "Yes" &&
+        'audits'[Full Name] IN VALUES('personnel'[Full Name]) &&
+        'audits'[Observation Date] >= LOOKUPVALUE('personnel'[Start Date], 'personnel'[Full Name], 'audits'[Full Name], 'personnel'[Start Date], 'audits'[Observation Date]) &&
+        'audits'[Observation Date] <= COALESCE(
+            LOOKUPVALUE('personnel'[End Date], 'personnel'[Full Name], 'audits'[Full Name], 'personnel'[Start Date], 'audits'[Observation Date]), 
+            TODAY()
+        ) &&
+        MAXX(
+            FILTER(
+                'personnel',
+                'personnel'[Full Name] = 'audits'[Full Name] &&
+                'audits'[Observation Date] >= 'personnel'[Start Date] &&
+                'audits'[Observation Date] <= COALESCE('personnel'[End Date], TODAY())
+            ),
+            'personnel'[Role]
+        ) = 'personnel'[Role]
+    )
+)
 
