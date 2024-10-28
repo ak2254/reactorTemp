a55c10e47c38e505fb4cc134d80378561747da48
ak2254 <51174202+ak2254@users.noreply.github.com>
2024-10-28 18:22:26 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,1 +1,22 @@
 i
+TotalObservations = 
+CALCULATE(
+    COUNTROWS('audits'), 
+    FILTER(
+        'audits',
+        'audits'[Completed] = "Yes" &&
+        'audits'[Full Name] IN VALUES('personnel'[Full Name]) &&
+        'audits'[Observation Date] >= 
+            CALCULATE(MIN('personnel'[Start Date]), 
+                      'personnel'[Full Name] = 'audits'[Full Name], 
+                      'audits'[Observation Date] >= 'personnel'[Start Date],
+                      'audits'[Observation Date] <= COALESCE('personnel'[End Date], TODAY())
+            ) &&
+        'audits'[Observation Date] <= 
+            CALCULATE(MAX(COALESCE('personnel'[End Date], TODAY())), 
+                      'personnel'[Full Name] = 'audits'[Full Name], 
+                      'audits'[Observation Date] >= 'personnel'[Start Date],
+                      'audits'[Observation Date] <= COALESCE('personnel'[End Date], TODAY())
+            )
+    )
+)
