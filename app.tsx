d965403d7cbcedbb104348c9cb855759f57294d6
ak2254 <51174202+ak2254@users.noreply.github.com>
2025-01-18 19:24:12 -0500

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -12,3 +12,21 @@ VAR QualifiedPeople =
 RETURN
 DIVIDE(QualifiedPeople, TotalPeople, 0)
 
+
+Qualified Leaders % by Role = 
+VAR TotalPeople = 
+    DISTINCTCOUNT(Audits[Name])
+
+VAR QualifiedPeopleByRole = 
+    CALCULATE(
+        DISTINCTCOUNT(Personnel[Name]),
+        FILTER(
+            Personnel,
+            Personnel[Date Qualified Start] <= MAX(DateTable[Date])
+        )
+    )
+
+RETURN
+DIVIDE(QualifiedPeopleByRole, TotalPeople, 0)
+
+
