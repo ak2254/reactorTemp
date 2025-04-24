54819e8a1af0c171a5f78973fb4c509a76f7baae
ak2254 <51174202+ak2254@users.noreply.github.com>
2025-04-24 10:24:42 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,14 +1,8 @@
-Subject: Initial Draft for Calendar View
-
-Hi Everyone,
-
-Sharing the initial draft we’ve created for the calendar view. Please take a look and let us know your feedback.
-
-We do have a few questions:
-
-Can you please confirm all the work types we have mapped so far?
-What tooltips would you like to see in this view?
-What colors should be associated with each work type? Currently, we have three colors—please let us know if any changes are needed.
-Please let us know if anything else is required or if any modifications are needed.
-
-Looking forward to your thoughts!
+UniquePersonRoles =
+SUMMARIZE(
+    PersonnelData,
+    PersonnelData[PersonName],
+    PersonnelData[RoleName],
+    "TargetAudits", MAX(PersonnelData[TargetAudits]),
+    "CompletedAudits", MAX(PersonnelData[CompletedAudits])
+)
