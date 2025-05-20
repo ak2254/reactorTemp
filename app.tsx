748714822880ebfb7cd7c3e509393c473c00e3cb
ak2254 <51174202+ak2254@users.noreply.github.com>
2025-05-20 18:37:31 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,11 +1,31 @@
 KPI % = 
-VAR SelectedQuestion = SELECTEDVALUE(QuestionTable[Question])
-RETURN
+VAR selectedQuestion = SELECTEDVALUE(QuestionTable[Question])
+VAR target = 
     SWITCH(
-        SelectedQuestion,
-        "OOT Requests Created", DIVIDE([OOT Requests], [Target WO], 0),
-        "Open OOT WOs > 30 Days", 
-            VAR ootCount = [OOT Open > 30 Days]
-            VAR target = [Target WO]
-            RETURN 1 - DIVIDE(ootCount, target + ootCount)
+        selectedQuestion,
+        "OOT Requests Created", 8,
+        "Open OOT WOs > 30 Days", 0
     )
+VAR result = 
+    SWITCH(
+        selectedQuestion,
+        "OOT Requests Created",
+            CALCULATE(
+                DIVIDE(COUNTROWS(WorkOrders), target, 0),
+                TREATAS(VALUES(DateTable[Date]), WorkOrders[CreateDate]),
+                WorkOrders[WorkOrderType] = "OOT"
+            ),
+        "Open OOT WOs > 30 Days",
+            VAR openCount = CALCULATE(
+                COUNTROWS(WorkOrders),
+                TREATAS(VALUES(DateTable[Date]), WorkOrders[CreateDate]),
+                WorkOrders[WorkOrderType] = "OOT",
+                WorkOrders[Status] = "Open",
+                DATEDIFF(WorkOrders[CreateDate], TODAY(), DAY) > 30
+            )
+            RETURN
+                1 - DIVIDE(openCount, target + openCount)
+    )
+RETURN
+    result
+
