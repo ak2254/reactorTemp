f8077cb79aaede8a03a68f447387bced373bc7b6
ak2254 <51174202+ak2254@users.noreply.github.com>
2025-06-04 11:25:49 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,17 +1,4 @@
- for row in data:
-        try:
-            approval_date = datetime.strptime(row["approval_date"], "%Y-%m-%d")
-            approval_due_date = datetime.strptime(row["approval_due_date"], "%Y-%m-%d")
-
-            submission_date_str = row.get("submission_date")
-            if submission_date_str:
-                submission_date = datetime.strptime(submission_date_str, "%Y-%m-%d")
-                if approval_date > submission_date:
-                    month_key = approval_due_date.strftime("%b %Y")
-                    monthly_counts[month_key] += 1
-                    used_records.append(row)
-            else:
-                if approval_date > datetime.today():
-                    month_key = approval_due_date.strftime("%b %Y")
-                    monthly_counts[month_key] += 1
-                    used_records.append(row)
+SELECT *
+FROM capa
+WHERE LOWER(CONCAT(SUBSTRING_INDEX(capa.assigned_to_first_name, ' ', 1), ' ', capa.assigned_to_lastname))
+      IN ('anjali smith', 'brian lee', 'sophie brown');
