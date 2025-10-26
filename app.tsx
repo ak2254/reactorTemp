13b4a575e26cfa62267a9f4cf0e2da65a693af86
ak2254 <51174202+ak2254@users.noreply.github.com>
2025-10-26 19:53:00 -0400

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,3 +1,7 @@
 =LET(txt,A2,
 digits, TEXTJOIN("",,IF(ISNUMBER(--MID(txt,SEQUENCE(LEN(txt)),1)),MID(txt,SEQUENCE(LEN(txt)),1),"")),
 digits)
+
+
+=LEFT(A2, MIN(FIND({0,1,2,3,4,5,6,7,8,9}&" ",A2&" "))-1)
+
